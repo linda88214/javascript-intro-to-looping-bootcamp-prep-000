faeba1be73add7b1e730f2cf70faa973b52ede91
Yerim Hu <yerimhu88@gmail.com>
2017-10-15 20:28:22 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    array.push('I am ${1} strange loop ${i===0 ? \'\' : \'s\'}.')
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log (--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+    return array;
+  } while (array.length > 0 && maybeTrue() );
+}
