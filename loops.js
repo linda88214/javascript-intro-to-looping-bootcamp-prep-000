87699528275e09acb1a2d407d4ea3afb7b1d8ad4
Yerim Hu <yerimhu88@gmail.com>
2017-10-24 02:01:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
+    if(i===0) {
+      return array = "I am 1 strange loop"
+    } else {
     array.push('I am ${1} strange loop ${i===0 ? \'\' : \'s\'}.')
+  }
   }
   return array
 }
