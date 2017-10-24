a3433f463f7c85b1eb5d5d3405e342b861205968
Yerim Hu <yerimhu88@gmail.com>
2017-10-24 02:02:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if(i===0) {
       return array = "I am 1 strange loop"
-    } else {
+    } else(i<25) {
     array.push('I am ${1} strange loop ${i===0 ? \'\' : \'s\'}.')
   }
   }
